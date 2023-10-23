6655f414071d3b5e8b0a66c3fc466fa7f8f27d2e
Dániel Kovács <daniel@kovacs.xyz>
2023-10-23 08:50:13 +0200

    Correct maven stages and add Sonar support

--- rc-shellmagick.d/C00-aliases-maven.sh
+++ rc-shellmagick.d/C00-aliases-maven.sh
@@ -12,25 +12,32 @@ echo "[2m[3mRegistering aliases for Maven[0m"
 # mv is taken 🙄
 alias mav='mvn verify'
 alias mavskip='mvn verify -DskipTests=true'
-alias macv='mvn clean verify'
+alias macv='mvn clean verify -Punit-tests'
 alias macvskip='mvn clean verify -DskipTests=true'
 
 alias mdl='mvn dependency:list'
 alias mdt='mvn dependency:tree'
 
-alias mi='mvn install'
+alias mi='mvn install -Punit-tests'
 alias miskip='mvn install -DskipTests=true'
-alias mci='mvn clean install'
+alias mci='mvn clean install -Punit-tests'
 alias mciskip='mvn clean install -DskipTests=true'
 
 alias mwasp='mvn dependency-check:check -Powasp-dependency-check'
-alias mtest='mvn test'
-alias mtestint='mvn test -Pintegration-tests'
-alias mtestpit='mvn test -Pmutation-tests org.pitest:pitest-maven:mutationCoverage'
+alias mtest='mvn test -Punit-tests'
+alias mtestint='mvn verify -Pintegration-tests'
+alias mtestpit='mvn verify -Pmutation-tests org.pitest:pitest-maven:mutationCoverage'
 alias pitreport='cygstart target/pit-reports/index.html'
 
 alias mdown='mvn dependency:sources dependency:resolve'
 alias mdowndoc='mvn depenedency:sources dependency:resolve -Dclassifier=javadoc'
+
+if [[ ! -z ${SONAR_HOST_URL+x} ]]; then
+	if [[ ! -z ${SONAR_TOKEN+x} ]]; then
+		alias msonar='mvn sonar:sonar -Dsonar.host.url=${SONAR_HOST_URL} -Dsonar.token=${SONAR_TOKEN} -Dsonar.branch.name=$(git rev-parse --abbrev-ref HEAD)'
+	fi
+fi
+
 
 # Note: '*' port is needed for JDK 11 and above
 alias debugmavensuspend='export MAVEN_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:${MAVEN_DEBUG_PORT}"'
